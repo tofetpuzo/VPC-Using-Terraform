33cccfd2c028cd4c16dbea3ebe053f08cd4c1546
tofetpuzo <topetemiola09@gmail.com>
2022-03-20 20:26:32 +0000

    using the terraform to build vpc

--- main.tf
+++ main.tf
@@ -122,10 +122,25 @@ resource "aws_security_group" "allow_web" {
 
 
 
-//7. Create a network interface with sn ip in the subner that was created in step 4
+//7. Create a network interface with an ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-01.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+  /* attachment {
+    instance     = aws_instance.test.id
+    device_index = 1
+  } */
+}
 
 
 //8. Assign an elastic IP to the network created in step 7
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+}
 
 
 //9. Create Ubuntu server and install/enable apache2
